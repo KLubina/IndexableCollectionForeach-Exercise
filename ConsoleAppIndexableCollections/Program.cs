a3054883c04c2339c75367cfb9df07a011fb1f69
Kristian Lubina <kristian.lubina@outlook.com>
2025-04-16 10:10:04 +0200

    Add files via upload

---
+++ ConsoleAppIndexableCollections/Program.cs
@@ -0,0 +1,16 @@
+﻿//from here
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/collections#indexable-collections
+
+//create a list
+List<string> salmons = [ "chinook", "coho", "pink", "sockeye" ];
+
+//remove coho from list
+salmons.Remove( "coho" );
+
+//iterate through the list
+foreach (var salmon in salmons )
+  {
+  Console.Write( salmon + " " );
+  }
+
+//output: chinook pink sockeye
